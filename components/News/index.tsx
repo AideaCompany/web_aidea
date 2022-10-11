6bd6ba7df82f0b38835f1ac2bf4a445cf3a66802
camilo <camilo.acnhiquer@gmail.com>
2022-10-11 13:21:14 -0500

    maquetación

--- components/News/index.tsx
+++ components/News/index.tsx
@@ -4,7 +4,14 @@ import { IProps } from "./props.interface"
 
 const News: FC<IProps> = () => {
   return (
-   
+    <div className="main_news">
+      <div className="tittle_news">
+        <h3> Enterate de nuestras noticias</h3>
+      </div>
+      <div className="Button_news">
+      {cards.map(card => (
+          <div className="son_card">{card.name}</div>
+    </div>
   )
 }
 
